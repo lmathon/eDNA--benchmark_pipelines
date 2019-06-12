a7be950abc1bc4f211fa4e17f02a00f9ac6dfb33
peguerin <pierre-edouard.guerin@cefe.cnrs.fr>
2019-06-12 17:33:54 +0200

    download mega ref database

---
+++ 99_utils/mega_download/download_input_data.sh
@@ -0,0 +1,24 @@
+###############################################################################
+## download data from MEGA server
+
+## curl required
+## wget required
+## openssl required
+
+#URL="https://mega.nz/#"'!'"VXoiDIrA"'!'"FIFrPEm5SFEVar1TI9yiNiuflSwsNfUUPeQsMb9FnJo"
+
+
+URL="https://mega.nz/#"'!'"seonxSqJ"'!'"Alr84wbCCwJzRvMU25eMtYCKq11UOHTbHtFmm3k63xk"
+
+bash 99_utils/mega_download/mega_fetch.sh $URL > 99_utils/mega_download/mega_info.txt
+
+FILE_URL=`sed '1q;d' 99_utils/mega_download/mega_info.txt`
+FILE_NAME=`sed '2q;d' 99_utils/mega_download/mega_info.txt`
+HEX=`sed '3q;d' 99_utils/mega_download/mega_info.txt`
+RAW_HEX=`sed '4q;d' 99_utils/mega_download/mega_info.txt`
+
+wget -O "${FILE_NAME}" "${FILE_URL}"
+cat "${FILE_NAME}" | openssl enc -d -aes-128-ctr -K "${HEX}" -iv "${RAW_HEX}" > "${FILE_NAME}".new
+mv -f "${FILE_NAME}".new "${FILE_NAME}"
+unzip "${FILE_NAME}"
+mv ${FILE_NAME/.zip/} 00_Input_data/
