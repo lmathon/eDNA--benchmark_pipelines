7fe01ce8189c628dc1a7bda7b274cf8ea063ab07
peguerin <pierre-edouard.guerin@cefe.cnrs.fr>
2019-06-26 16:51:37 +0200

    debug fucking simulation grinder de merde

--- grinder_simulations/simulation_grinder.sh
+++ grinder_simulations/simulation_grinder.sh
@@ -1,6 +1,6 @@
 ID_SAMPLE=$1
 echo "Simulating sample${ID_SAMPLE}..." 
-grinder -rf grinder_simulations/Inputs/seq_grinder_simulations/Inputs/abund_sample"${ID_SAMPLE}".txt \
+grinder -rf seq_grinder_simulations/Inputs/abund_sample"${ID_SAMPLE}".txt \
 -id 150 -rd 150 -fq 1 -ql 36 30 -mo FR \
 -dc '-' -un 1 -md poly4 3e-3 3.3e-8 -mr 98 2 \
 -hd Balzer -cp 1 -ck 0 -mi grinder_simulations/Inputs/tag_sample"${ID_SAMPLE}".fasta \
