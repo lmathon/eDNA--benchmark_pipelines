10b9ce928c0b3a4a844b52e0d50506d8e465ea92
peguerin <pierre-edouard.guerin@cefe.cnrs.fr>
2019-06-26 15:27:02 +0200

    grinder parralel

--- grinder_simulations/simulation_grinder.sh
+++ grinder_simulations/simulation_grinder.sh
@@ -1,10 +1,10 @@
 ID_SAMPLE=$1
 echo "Simulating sample${ID_SAMPLE}..." 
 grinder -rf grinder_simulations/Inputs/seq_grinder_simulations/Inputs/abund_sample"${ID_SAMPLE}".txt \
-    -id 150 -rd 150 -fq 1 -ql 36 30 -mo FR \
-    -dc '-' -un 1 -md poly4 3e-3 3.3e-8 -mr 98 2 \
-    -hd Balzer -cp 1 -ck 0 -mi grinder_simulations/Inputs/tag_sample"${ID_SAMPLE}".fasta \
-    -bn sample"${ID_SAMPLE}" -od grinder_simulations/Outputs/sample"${ID_SAMPLE}"
+-id 150 -rd 150 -fq 1 -ql 36 30 -mo FR \
+-dc '-' -un 1 -md poly4 3e-3 3.3e-8 -mr 98 2 \
+-hd Balzer -cp 1 -ck 0 -mi grinder_simulations/Inputs/tag_sample"${ID_SAMPLE}".fasta \
+-bn sample"${ID_SAMPLE}" -od grinder_simulations/Outputs/sample"${ID_SAMPLE}"
 
 ## commande à mettre en boucle pour les échantillons 01 à 30 : copier les fichiers ranks.txt de chaque échantillon dans un folder ranks
 cp grinder_simulations/Outputs/sample"${ID_SAMPLE}"/*ranks.txt grinder_simulations/Outputs/ranks/
