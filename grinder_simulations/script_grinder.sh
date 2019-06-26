06f115902fb1422e8c7059a1f333ea24b835b517
peguerin <pierre-edouard.guerin@cefe.cnrs.fr>
2019-06-26 14:32:04 +0200

    grinder scripts

--- grinder_simulations/script_grinder.sh
+++ grinder_simulations/script_grinder.sh
@@ -4,17 +4,18 @@ CORES=16
 #### number of samples
 NB_SAMPLE=30
 #### 
-
+#MONTAGE=""
+MONTAGE="/simulations/"
 
 for ID_SAMPLE in `seq -w 1 $NB_SAMPLE`; 
 do echo "${ID_SAMPLE}" 
 ##  grinder command to run with input files into folder 'Inputs'
-grinder -rf grinder_simulations/Inputs/seq_sample"${ID_SAMPLE}".fasta \
-    -tr 90000 -nl 12 -di 0 -af grinder_simulations/Inputs/abund_sample"${ID_SAMPLE}".txt \
+grinder -rf "${MONTAGE}"grinder_simulations/Inputs/seq_sample"${ID_SAMPLE}".fasta \
+    -tr 90000 -nl 12 -di 0 -af "${MONTAGE}"grinder_simulations/Inputs/abund_sample"${ID_SAMPLE}".txt \
     -id 150 -rd 150 -fq 1 -ql 36 30 -mo FR \
     -dc '-' -un 1 -md poly4 3e-3 3.3e-8 -mr 98 2 \
-    -hd Balzer -cp 1 -ck 0 -mi grinder_simulations/Inputs/tag_sample"${ID_SAMPLE}".fasta \
-    -bn sample"${ID_SAMPLE}" -od grinder_simulations/Outputs/sample"${ID_SAMPLE}"
+    -hd Balzer -cp 1 -ck 0 -mi "${MONTAGE}"grinder_simulations/Inputs/tag_sample"${ID_SAMPLE}".fasta \
+    -bn sample"${ID_SAMPLE}" -od "${MONTAGE}"grinder_simulations/Outputs/sample"${ID_SAMPLE}"
 done
 
 """
