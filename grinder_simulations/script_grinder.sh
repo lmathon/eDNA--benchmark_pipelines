ac821f13782299e80541dec319893530809e5b4c
peguerin <pierre-edouard.guerin@cefe.cnrs.fr>
2019-06-26 14:47:21 +0200

    grinder scripts

--- grinder_simulations/script_grinder.sh
+++ grinder_simulations/script_grinder.sh
@@ -3,17 +3,17 @@
 CORES=16
 #### number of samples
 NB_SAMPLE=30
-#### 
-#MONTAGE=""
-MONTAGE="/simulations/"
+
 
 for ID_SAMPLE in `seq -w 1 $NB_SAMPLE`; 
 do echo "${ID_SAMPLE}" 
 ##  grinder command to run with input files into folder 'Inputs'
-grinder -rf "${MONTAGE}"grinder_simulations/Inputs/seq_sample"${ID_SAMPLE}".fasta \
-    -tr 90000 -nl 12 -di 0 -af "${MONTAGE}"grinder_simulations/Inputs/abund_sample"${ID_SAMPLE}".txt \
+grinder -rf grinder_simulations/Inputs/seq_grinder_simulations/Inputs/abund_sample"${ID_SAMPLE}".txt \
     -id 150 -rd 150 -fq 1 -ql 36 30 -mo FR \
     -dc '-' -un 1 -md poly4 3e-3 3.3e-8 -mr 98 2 \
-    -hd Balzer -cp 1 -ck 0 -mi "${MONTAGE}"grinder_simulations/Inputs/tag_sample"${ID_SAMPLE}".fasta \
-    -bn sample"${ID_SAMPLE}" -od "${MONTAGE}"grinder_simulations/Outputs/sample"${ID_SAMPLE}"
+    -hd Balzer -cp 1 -ck 0 -mi grinder_simulations/Inputs/tag_sample"${ID_SAMPLE}".fasta \
+    -bn sample"${ID_SAMPLE}" -od grinder_simulations/Outputs/sample"${ID_SAMPLE}"
+
+## commande à mettre en boucle pour les échantillons 01 à 30 : copier les fichiers ranks.txt de chaque échantillon dans un folder ranks
+cp grinder_simulations/Outputs/sample"${ID_SAMPLE}"/*ranks.txt grinder_simulations/Outputs/ranks/
 done
