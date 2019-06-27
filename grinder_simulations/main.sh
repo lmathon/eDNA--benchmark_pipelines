3fdd91fce2c727a846fdbd6446805fbdd117142d
peguerin <pierre-edouard.guerin@cefe.cnrs.fr>
2019-06-27 10:07:34 +0200

    deinterlave

--- grinder_simulations/main.sh
+++ grinder_simulations/main.sh
@@ -1,2 +1,2 @@
 ## run grinder 30 simulations 12 replicats
-singularity exec -B .:/simulations grinder.img bash -c "cd /simulations ; bash grinder_simulations/script_grinder.sh" 2> grinder_simulations/Logs/simulations_grinder.log
+singularity exec -B .:/simulations grinder.img bash -c "cd /simulations ; bash grinder_simulations/script_grinder.sh"
