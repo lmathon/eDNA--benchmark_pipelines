a50ce9633ce13470c451d7751bcd50419602b542
peguerin <pierre-edouard.guerin@cefe.cnrs.fr>
2019-06-27 13:04:10 +0200

    mega download fastq

--- 99_utils/mega_download/download_input_data.sh
+++ 99_utils/mega_download/download_input_data.sh
@@ -7,7 +7,7 @@
 
 #URL="https://mega.nz/#"'!'"VXoiDIrA"'!'"FIFrPEm5SFEVar1TI9yiNiuflSwsNfUUPeQsMb9FnJo"
 
-
+## download reference data_base
 URL="https://mega.nz/#"'!'"seonxSqJ"'!'"Alr84wbCCwJzRvMU25eMtYCKq11UOHTbHtFmm3k63xk"
 
 bash 99_utils/mega_download/mega_fetch.sh $URL > 99_utils/mega_download/mega_info.txt
@@ -22,3 +22,20 @@ cat "${FILE_NAME}" | openssl enc -d -aes-128-ctr -K "${HEX}" -iv "${RAW_HEX}" > 
 mv -f "${FILE_NAME}".new "${FILE_NAME}"
 unzip "${FILE_NAME}"
 mv ${FILE_NAME/.zip/} 00_Input_data/
+
+
+## download fastq.gz files
+
+URL="https://mega.nz/#F"'!'"wWRglCiT"'!'"fmEhEIJXdvBlNvjOAJ8YCA"
+
+bash 99_utils/mega_download/mega_fetch.sh $URL > 99_utils/mega_download/mega_info.txt
+
+FILE_URL=`sed '1q;d' 99_utils/mega_download/mega_info.txt`
+FILE_NAME=`sed '2q;d' 99_utils/mega_download/mega_info.txt`
+HEX=`sed '3q;d' 99_utils/mega_download/mega_info.txt`
+RAW_HEX=`sed '4q;d' 99_utils/mega_download/mega_info.txt`
+
+wget -O "${FILE_NAME}" "${FILE_URL}"
+cat "${FILE_NAME}" | openssl enc -d -aes-128-ctr -K "${HEX}" -iv "${RAW_HEX}" > "${FILE_NAME}".new
+mv -f "${FILE_NAME}".new "${FILE_NAME}"
+mv "${FILE_NAME}" 00_Input_data/
