bdf01c072e72eebbcd22d08ae125c498e081c3d8
peguerin <pierre-edouard.guerin@cefe.cnrs.fr>
2019-06-27 10:07:34 +0200

    deinterlave

--- grinder_simulations/script_grinder.sh
+++ grinder_simulations/script_grinder.sh
@@ -3,10 +3,16 @@
 CORES=16
 #### number of samples
 NB_SAMPLE=30
-
+NB_REPLICAT=12
 
 for ID_SAMPLE in `seq -w 1 $NB_SAMPLE`; do
 	((i=i%CORES)); ((i++==0)) && wait
 	##  grinder command to run with input files into folder 'Inputs'
-	bash grinder_simulations/simulation_grinder.sh "${ID_SAMPLE}" &
+	#bash grinder_simulations/simulation_grinder.sh "${ID_SAMPLE}" &
+	for ID_REPLICAT in `seq -w 1 $NB_REPLICAT`; do
+		bash deinterleave_fastq.sh < grinder_simulations/Outputs/sample"${ID_SAMPLE}"/sample-"${ID_REPLICAT}"-reads.fastq \
+		grinder_simulations/Outputs/grinder_teleo1/paired_end_R1/sample"${ID_SAMPLE}"-"${ID_REPLICAT}"_R1.fastq \
+		grinder_simulations/Outputs/grinder_teleo1/paired_end_R2/sample"${ID_SAMPLE}"-"${ID_REPLICAT}"_R2.fastq \
+		compress
+	done &
 done
