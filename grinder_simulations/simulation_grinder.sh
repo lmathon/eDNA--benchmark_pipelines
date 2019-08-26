e518bb056aca9ec64583ac0a9bebc592e5b72ec4
root <root@EB10PA4.newcefe.newage.fr>
2019-08-27 00:50:52 +0200

    modified grinder script

--- grinder_simulations/simulation_grinder.sh
+++ grinder_simulations/simulation_grinder.sh
@@ -4,7 +4,7 @@ grinder -rf grinder_simulations/Inputs/seq_sample"${ID_SAMPLE}".fasta \
     -tr 90000 -nl 12 -di 0 -af grinder_simulations/Inputs/abund_sample"${ID_SAMPLE}".txt \
     -id 150 -rd 150 -fq 1 -ql 36 30 -mo FR \
     -dc '-' -un 1 -md poly4 3e-3 3.3e-8 -mr 98 2 \
-    -hd Balzer -cp 1 -ck 0 -mi grinder_simulations/Inputs/tag_sample"${ID_SAMPLE}".fasta \
+    -cp 1 -ck 0 -mi grinder_simulations/Inputs/tag_sample"${ID_SAMPLE}".fasta \
     -bn sample"${ID_SAMPLE}" -od grinder_simulations/Outputs/sample"${ID_SAMPLE}"/
 
 
