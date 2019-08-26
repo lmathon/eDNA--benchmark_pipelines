f7ed3ebc234c634baa7df6f34cba2e2e0df9c162
peguerin <pierre-edouard.guerin@cefe.cnrs.fr>
2019-08-26 15:52:26 +0200

    qsub SGE bash utils

---
+++ 99_utils/submitjob_sge_cluster/bash2sge.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+##=============================================================================
+## HEADER
+##=============================================================================
+## USAGE
+##    subbash monscript.sh
+##
+## DESCRIPTION
+##    write a SGE script to submit a given bash script with `qsub`
+##    This SGE file can be submitted with `qsub` on MBB SGE cluster
+##
+##    We use a template file `template_qsub.sh` to write a base of SGE script
+##    with default parameters and values. We add bash command running the bash
+##    script defined as first argument `monscript.sh`
+##
+##    SGE script will be written into folder 99_utils/submitjob_sge_cluster/qsub_scripts/
+##    SGE script name contains name of users, name of bash script to submit
+##    and date of creation.
+##
+##    By default this SGE script will submit the job to `cemeb20.q` queue 
+##    with multithread60 16 cores
+##    SGE's ouputs or error are defined to be written into folder
+##    99_utils/submitjob_sge_cluster/qsub_outputs/
+##    
+##
+##
+##=============================================================================
+## END_OF_HEADER
+##=============================================================================
+
+template_script="99_utils/submitjob_sge_cluster/template_qsub.sh"
+qsub_script="99_utils/submitjob_sge_cluster/qsub_scripts/"
+run_output="99_utils/submitjob_sge_cluster/qsub_outputs/"
+nom_bash_script=`echo $1 | cut -d "." -f 1`
+location_bash_script=`pwd`
+nom_qsub_script=$USER"_"$nom_bash_script"_"$(date +%Y%m%d-%H%M%S)
+sed -e "s/JOB_NAME/$nom_qsub_script/g" -e "s|OUT_FILE|$run_output/$nom_qsub_script\.out|g" -e "s|ERR_FILE|$run_output/$nom_qsub_script\.err|g" $template_script  > $qsub_script/temp_script.sh
+echo -e "bash $location_bash_script/$nom_bash_script.sh\n" >> $qsub_script/temp_script.sh
+mv $qsub_script/temp_script.sh $qsub_script/$nom_qsub_script".sh"
+echo $qsub_script/$nom_qsub_script".sh is ready to be qsubmitted."
+#qsub $qsub_script/$nom_qsub_script".sh"
---
+++ 99_utils/submitjob_sge_cluster/template_qsub.sh
@@ -0,0 +1,23 @@
+# Job name
+#$ -N JOB_NAME
+# Using current working directory (otherwise, you will have to use '#$ wd /path/to/run')
+#$ -cwd
+# job time limits (h_rt is required [s_rt == software time limit / h_rt == hardware time limit])
+#$ -l s_rt=999:55:00
+#$ -l h_rt=920:00:00
+# choose to run on a specific queue
+# (qconf -sql (to list queues) qconf -sq queue_name (to print informations on this queue))
+#$ -q cemeb20.q
+# Get a mail when the job begins, ends or is suspended
+#$ -m ebs
+#$ -M me@mail.com
+# Redirects the standard output to the named file.
+#$ -o OUT_FILE
+##$ -e ERR_FILE
+# merge standard and error outputs
+#$ -j y
+# choose a parallel environment and run on 60 slots (use $PE_HOSTFILE)
+#$ -pe multithread60 16
+# Export all my environment variables into job runtime context
+#$ -V
+###################
