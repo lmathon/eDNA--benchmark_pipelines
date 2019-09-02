312c6fee6c6b62e0d456b7c883b0a7e27e10ea3c
peguerin <pierre-edouard.guerin@cefe.cnrs.fr>
2019-09-02 13:55:36 +0200

    add config

---
+++ 98_infos/config.sh
@@ -0,0 +1,17 @@
+###############################################################################
+##
+## The config.sh file contains modifiable parameters (and non modifiable too).
+## In normal circumstances it should be the only file which may be edited 
+## by the end-user. 
+##
+###############################################################################
+##
+###############################################################################
+## singularity containers path
+
+## mbb
+EDNATOOLS_SIMG="/share/reservebenefit/utils/conteneurs/ednatools.simg"
+OBITOOLS_SIMG="/share/reservebenefit/utils/conteneurs/obitools.simg"
+
+
+###############################################################################
