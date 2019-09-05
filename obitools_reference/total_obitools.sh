c0e8863b884a0a6ea0b708d4bc81f094ad840f98
peguerin <pierre-edouard.guerin@cefe.cnrs.fr>
2019-09-05 09:43:51 +0200

    debug obitools reference scripts

--- obitools_reference/total_obitools.sh
+++ obitools_reference/total_obitools.sh
@@ -22,7 +22,7 @@ step=total_obitools
 R1_fastq="$DATA_PATH"/"$pref"_R1.fastq.gz
 R2_fastq="$DATA_PATH"/"$pref"_R2.fastq.gz
 # Chemin vers le fichier 'tags.txt'
-sample_description_file='/share/reservebenefit/working/lmathon/eDNA--benchmark_pipelines/00_Input_data/sample_description_file.txt'
+sample_description_file='/share/reservebenefit/working/pierre/eDNA--benchmark_pipelines/00_Input_data/sample_description_file.txt'
 # Chemin vers le fichier 'db_sim_teleo1.fasta'
 refdb_dir='/share/reservebenefit/working/reference_database/db_sim_teleo1.fasta'
 # Chemin vers les fichiers 'embl' de la base de référence
@@ -30,8 +30,8 @@ base_dir='/share/reservebenefit/working/reference_database/'
 ### Les préfixes des fichiers de la base de ref ne doivent pas contenir "." ou "_"
 base_pref=`ls $base_dir/*sdx | sed 's/_[0-9][0-9][0-9].sdx//'g | awk -F/ '{print $NF}' | uniq`
 # Chemin vers les répertoires de sorties intermédiaires et finales
-main_dir='/share/reservebenefit/working/lmathon/eDNA--benchmark_pipelines/obitools_reference/outputs_obitools/main'
-fin_dir='/share/reservebenefit/working/lmathon/eDNA--benchmark_pipelines/obitools_reference/outputs_obitools/final'
+main_dir='/share/reservebenefit/working/pierre/eDNA--benchmark_pipelines/obitools_reference/outputs_obitools/main'
+fin_dir='/share/reservebenefit/working/pierre/eDNA--benchmark_pipelines/obitools_reference/outputs_obitools/final'
 
 
 ################################################################################################
