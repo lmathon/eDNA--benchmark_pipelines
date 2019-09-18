9857a0d7e37795099d521dfa7718ff9ddd630820
peguerin <pierre-edouard.guerin@cefe.cnrs.fr>
2019-09-18 16:01:32 +0200

    tiny data

--- 98_infos/config.sh
+++ 98_infos/config.sh
@@ -35,7 +35,11 @@ SINGULARITY_EXEC_CMD="singularity exec --bind /share:/share"
 
 ## path to folder of reads forward et reverse
 #DATA_PATH=`pwd`"/00_Input_data/"
-DATA_PATH="/share/reservebenefit/working/Input_data/Outputs"
+## path to data to test
+#DATA_PATH="/share/reservebenefit/working/Input_data/Outputs"
+## path to tiny data only for debug
+DATA_PATH="/share/reservebenefit/working/Input_data/tiny"
+
 INPUT_DATA=`pwd`"/00_Input_data/"
 
 ## path to reference database folder
