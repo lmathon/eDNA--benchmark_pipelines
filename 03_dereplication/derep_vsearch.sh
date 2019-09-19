30b0499622c961993543782c0cea36d6db7cb25a
peguerin <pierre-edouard.guerin@cefe.cnrs.fr>
2019-09-19 15:04:41 +0200

    correct path

--- 03_dereplication/derep_vsearch.sh
+++ 03_dereplication/derep_vsearch.sh
@@ -54,8 +54,8 @@ base_dir=${REFDB_PATH}
 ### remove '.' and  '_' from the prefix files
 base_pref=`ls $base_dir/*sdx | sed 's/_[0-9][0-9][0-9].sdx//'g | awk -F/ '{print $NF}' | uniq`
 ## path to outputs final and temporary (main)
-main_dir=`pwd`"03_dereplication/Outputs/01_vsearch/main"
-fin_dir=`pwd`"03_dereplication/Outputs/01_vsearch/final"
+main_dir=`pwd`"/03_dereplication/Outputs/01_vsearch/main"
+fin_dir=`pwd`"/03_dereplication/Outputs/01_vsearch/final"
 
 
 
