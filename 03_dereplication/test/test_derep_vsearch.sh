e7dda494a5d6d99b2aacddf24ac5036e56b82741
peguerin <pierre-edouard.guerin@cefe.cnrs.fr>
2019-10-03 09:59:58 +0200

    add test allvsearch_into_obifasta.py

---
+++ 03_dereplication/test/test_derep_vsearch.sh
@@ -0,0 +1,11 @@
+source 98_infos/config.sh
+
+container_python2=${SINGULARITY_EXEC_CMD}" "${EDNATOOLS_SIMG}" python2"
+
+
+### test allvsearch_into_obitfasta.py
+INFO_SEQ=$(pwd)/03_dereplication/test/01_vsearch/grinder_teleo1_all_sample_clean.uniq.fasta
+ALL_SAMPLE_SEQ_UNIQ=$(pwd)/03_dereplication/test/01_vsearch/grinder_teleo1_all_sample_clean.uniq.fasta
+FORMATED=$(pwd)/03_dereplication/test/01_vsearch/FORMATED.fasta
+
+$container_python2 03_dereplication/allvsearch_into_obifasta.py -i $INFO_SEQ -f $all_sample_sequences_uniq -o $FORMATED
