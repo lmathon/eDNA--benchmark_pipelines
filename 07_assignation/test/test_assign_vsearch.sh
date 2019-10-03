7932fe9d60730d97271605b62030b25adc06fe2c
peguerin <pierre-edouard.guerin@cefe.cnrs.fr>
2019-10-03 14:14:43 +0200

    fix test assign vsearch

--- 07_assignation/test/test_assign_vsearch.sh
+++ 07_assignation/test/test_assign_vsearch.sh
@@ -2,10 +2,10 @@ source 98_infos/config.sh
 vsearch=${SINGULARITY_EXEC_CMD}" "${EDNATOOLS_SIMG}" vsearch"
 
 refdb_dir=`pwd`"/07_assignation/db_teleo_vsearch.fasta"
-all_sample_sequences_sort=`pwd`"07_assignation/test/01_vsearch/grinder_teleo1_all_sample_clean.uniq.ann.sort.fasta"
+all_sample_sequences_sort=`pwd`"/07_assignation/test/01_vsearch/grinder_teleo1_all_sample_clean.uniq.ann.sort.fasta"
 
 
-ASSIGNED=`pwd`"07_assignation/test/01_vsearch/ASSIGNED"
+ASSIGNED=`pwd`"/07_assignation/test/01_vsearch/ASSIGNED"
 
 ## test assignation vsearch
 
