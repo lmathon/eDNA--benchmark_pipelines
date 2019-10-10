4012594605d08d4e743e855f0859d08f1541173d
peguerin <pierre-edouard.guerin@cefe.cnrs.fr>
2019-10-10 16:19:29 +0200

    convert vsearch output into obifasta

--- 07_assignation/convert_assign_vsearch_2_obifasta.py
+++ 07_assignation/convert_assign_vsearch_2_obifasta.py
@@ -34,11 +34,7 @@ from Bio.SeqRecord import SeqRecord
 #CLASS
 #===============================================================================
 
-class Obinfo:
-     def __init__(self,id,merged_sample,count):
-         self.id=id
-         self.merged_sample=merged_sample
-         self.count=count
+## nada
 
 #===============================================================================
 #ARGUMENTS
@@ -60,17 +56,37 @@ vsearchFile = args.vsearch_assignation
 outputFile = args.output
 obitFastaFile = args.obifasta
 
-#vsearchFile="cattest_derep"
-#outputFile="cattest.obifasta"
-#infoSeqFile="info_seq"
-
-
-
-with open(vsearchFile,'r') as readFile:    
+##  read vsearch assignement --blast6out
+dicOfseq={}
+with open(vsearchFile,'r') as readFile:
+    pair=0;
     for ligne in readFile.readlines():
-        if ligne.startswith(">"):
-            ligneSplit=ligne[1:].split(";")
-            propreligne=ligneSplit[0].replace(' ',';')+"; "+"; ".join(str(elem) for elem in ligneSplit[1:])
-            print propreligne
+        if pair !=1:
+            ligneSplit=ligne.split(";")
+            ## edit ID tax best match
+            bestmatch=ligneSplit[3].replace(" ","")
+            ligneSplit[3]="best_match={'db_teleo1': '"+bestmatch+"'}"
+            ## edit best_identity
+            best_identity=ligneSplit[-1].split()[0]
+            ligneSplit[-1]="{'db_teleo1': "+best_identity+"}"
+            lignePropre="; ".join(str(elem) for elem in ligneSplit[0:])+";"
+            dicOfseq[ligneSplit[0].split(" ")[0]]=lignePropre
+            pair=1
+        else:
+            pair=0
 
+print dicOfseq
 
+mes_records=[]
+for seq_record in SeqIO.parse(obitFastaFile, "fasta", alphabet=IUPAC.unambiguous_dna):
+    seq_record_DescriptionSplit=seq_record.description.split(";")     
+    vSeqId=str(seq_record_DescriptionSplit[0].split(" ")[0])    
+    if vSeqId in dicOfseq:
+        print vSeqId
+        local_id=dicOfseq[vSeqId]
+        local_seq=str(repr(str(seq_record.seq.lower()))).replace("'","")
+        local_record=SeqRecord(Seq(local_seq,IUPAC.unambiguous_dna), id=local_id,description="")
+        mes_records.append(local_record)
+        ## remove the key (we need only one sequence by key: the "seed" sequence)
+        del dicOfseq[vSeqId]
+SeqIO.write(mes_records, outputFile, "fasta")
