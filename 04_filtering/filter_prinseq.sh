9c221beb9e38088ba5ecf615ec1e428fad10fa8d
root <root@EB10PA4.newcefe.newage.fr>
2019-10-27 16:58:32 +0100

    update scripts filtering

--- 04_filtering/filter_prinseq.sh
+++ 04_filtering/filter_prinseq.sh
@@ -1,50 +1,75 @@
 #!/bin/bash
-##vsearch
+###############################################################################
+## Codes for the paper:
+##   ..............
+##
+## Authors : GUERIN Pierre-Edouard, MATHON Laetitia
+## Montpellier 2019-2020
+## 
+###############################################################################
+## Usage:
+##    bash obitools_reference/total_obitools.sh
+##
+## Description:
+##  ..............    
+##
+##
+##
+###############################################################################
+## load config global variables
+source 98_infos/config.sh
 
-illuminapairedend='singularity exec obitools.img illuminapairedend'
-obigrep='singularity exec obitools.img obigrep'
-ngsfilter='singularity exec obitools.img ngsfilter'
-obisplit='singularity exec obitools.img obisplit'
-obiuniq='singularity exec obitools.img obiuniq'
-prinseq='perl prinseq-lite-0.20.4/prinseq-lite.pl'
-obiannotate='singularity exec obitools.img obiannotate'
-obiclean='singularity exec obitools.img obiclean'
-ecotag='singularity exec obitools.img ecotag'
-obisort='singularity exec obitools.img obisort'
-obitab='singularity exec obitools.img obitab'
 
-# Chemin vers répertoire contenant les reads forward et reverse
-DATA_PATH='00_Input_data/forward_reverse_reads'
-# Prefixe pour tous les fichiers générés
-pref=grinder_teleo1
-# Prefixe du tableau final, contenant l'étape et le programme testé (ex: merging_obitools) 
-step=filter_prinseq
-# Fichiers contenant les reads forward et reverse
-R1_fastq="$DATA_PATH"/"$pref"_R1.fastq
-R2_fastq="$DATA_PATH"/"$pref"_R2.fastq
-# Chemin vers le fichier 'tags.txt'
-sample_description_file='00_Input_data/sample_description_file.txt'
-# Chemin vers le fichier 'db_sim_teleo1.fasta'
-refdb_dir='00_Input_data/reference_database/db_sim_teleo1.fasta'
-# Chemin vers les fichiers 'embl' de la base de référence
-base_dir='00_Input_data/reference_database'
-### Les préfixes des fichiers de la base de ref ne doivent pas contenir "." ou "_"
+## Obitools
+illuminapairedend=${SINGULARITY_EXEC_CMD}" "${OBITOOLS_SIMG}" illuminapairedend"
+obigrep=${SINGULARITY_EXEC_CMD}" "${OBITOOLS_SIMG}" obigrep"
+ngsfilter=${SINGULARITY_EXEC_CMD}" "${OBITOOLS_SIMG}" ngsfilter"
+obisplit=${SINGULARITY_EXEC_CMD}" "${OBITOOLS_SIMG}" obisplit"
+obiuniq=${SINGULARITY_EXEC_CMD}" "${OBITOOLS_SIMG}" obiuniq"
+obiannotate=${SINGULARITY_EXEC_CMD}" "${OBITOOLS_SIMG}" obiannotate"
+obiclean=${SINGULARITY_EXEC_CMD}" "${OBITOOLS_SIMG}" obiclean"
+ecotag=${SINGULARITY_EXEC_CMD}" "${OBITOOLS_SIMG}" ecotag"
+obisort=${SINGULARITY_EXEC_CMD}" "${OBITOOLS_SIMG}" obisort"
+obitab=${SINGULARITY_EXEC_CMD}" "${OBITOOLS_SIMG}" obitab"
+## EDNAtools
+prinseq=${SINGULARITY_EXEC_CMD}" "${EDNATOOLS_SIMG}" prinseq"
+container_python2=${SINGULARITY_EXEC_CMD}" "${EDNATOOLS_SIMG}" python2"
+
+
+## Prefix for all generated files
+pref="grinder_teleo1"
+## Prefix of the final table, including the step and the program tested (ie: merging_obitools)
+step="filter_prinseq"
+## Path to the directory containing forward and reverse reads
+R1_fastq="${DATA_PATH}"/"$pref"/"$pref"_R1.fastq
+R2_fastq="${DATA_PATH}"/"$pref"/"$pref"_R2.fastq
+## path to 'sample_description_file.txt'
+sample_description_file=${INPUT_DATA}"/sample_description_file.txt"
+## path to the file 'db_sim_teleo1.fasta'
+refdb_dir=${REFDB_PATH}"/db_teleo1.fasta"
+## Path to embl files of the reference database
+base_dir=${REFDB_PATH}
+### remove '.' and  '_' from the prefix files
 base_pref=`ls $base_dir/*sdx | sed 's/_[0-9][0-9][0-9].sdx//'g | awk -F/ '{print $NF}' | uniq`
-# Chemin vers les répertoires de sorties intermédiaires et finales
-main_dir='04_filtering/Outputs/04_prinseq/main'
-fin_dir='04_filtering/Outputs/04_prinseq/final'
+## path to outputs final and temporary (main)
+main_dir=`pwd`"/04_filtering/Outputs/04_prinseq/main"
+fin_dir=`pwd`"/04_filtering/Outputs/04_prinseq/final"
 
 
 ################################################################################################
 
-# Assemblage des reads forward et reverse
-$illuminapairedend -r $R2_fastq $R1_fastq > $main_dir/"$pref".fastq
-# Supression des reads non alignés
-$obigrep -p 'mode!="joined"' $main_dir/"$pref".fastq > $main_dir/"$pref".ali.fastq
-# Assignation de chaque séquence à son échantillon
-$ngsfilter -t $sample_description_file -u $main_dir/"$pref"_unidentified.fastq $main_dir/"$pref".ali.fastq --fasta-output > $main_dir/"$pref".ali.assigned.fasta
-# Séparation du fichier global en un fichier par échantillon 
-$obisplit -p $main_dir/"$pref"_sample_ -t sample --fasta $main_dir/"$pref".ali.assigned.fasta.gz
+## forward and reverse reads assembly
+assembly=${main_dir}"/"${pref}".fastq"
+$illuminapairedend -r ${R2_fastq} ${R1_fastq} > ${assembly}
+## Remove non-aligned reads
+assembly_ali="${assembly/.fastq/.ali.fastq}"
+$obigrep -p 'mode!="joined"' ${main_dir}"/"${pref}".fastq" > ${assembly_ali}
+## Assign each sequence to a sample
+identified="${assembly_ali/.ali.fastq/.ali.assigned.fasta}"
+unidentified="${assembly_ali/.ali.fastq/_unidentified.fastq}"
+$ngsfilter -t ${sample_description_file} -u ${unidentified} ${assembly_ali} --fasta-output > ${identified}
+## Split big file into one file per sample 
+$obisplit -p $main_dir/"$pref"_sample_ -t sample --fasta ${identified}
 
 all_samples_parallel_cmd_sh=$main_dir/"$pref"_sample_parallel_cmd.sh
 echo "" > $all_samples_parallel_cmd_sh
@@ -52,27 +77,27 @@ for sample in `ls $main_dir/"$pref"_sample_*.fasta`;
 do
   sample_sh="${sample/.fasta/_cmd.sh}"
   echo "bash "$sample_sh >> $all_samples_parallel_cmd_sh
-  # Déréplication des reads en séquences uniques
+  # Dereplicate reads in unique sequences
   dereplicated_sample="${sample/.fasta/.uniq.fasta}"
   echo "$obiuniq -m sample "$sample" > "$dereplicated_sample > $sample_sh;
-  # On garde les séquences de plus de 20pb sans bases ambigues
+  # Keep only sequences longer than 20pb with no N bases
   good_sequence_sample="${dereplicated_sample/.fasta/.l20.fasta}"
   echo "/usr/bin/time $prinseq -fasta "$dereplicated_sample" -min_len 20 -ns_max_n 0 -noniupac --out_format 1 -out_good "$good_sequence_sample >> $sample_sh
-  # Supression des erreurs de PCR et séquençage (variants)
+  # Removal of PCR and sequencing errors (variants)
   clean_sequence_sample="${good_sequence_sample/.fasta./.r005.clean.fasta}"
   echo "$obiclean -r 0.05 -H "$good_sequence_sample" > "$clean_sequence_sample >> $sample_sh
 done
 parallel < $all_samples_parallel_cmd_sh
-#Concatenation de tous les échantillons en un fichier
+# Concatenate all files into one main file
 all_sample_sequences_clean=$main_dir/"$pref"_all_sample_clean.fasta
 cat $main_dir/"$pref"_sample_*.uniq.l20.r005.clean.fasta > $all_sample_sequences_clean
-# Déréplication en séquences uniques
+# Dereplicate in unique sequences
 all_sample_sequences_uniq="${all_sample_sequences_clean/.fasta/.uniq.fasta}"
 $obiuniq -m sample $all_sample_sequences_clean > $all_sample_sequences_uniq
-# Assignation taxonomique
+# Taxonomic assignation
 all_sample_sequences_tag="${all_sample_sequences_uniq/.fasta/.tag.fasta}"
 $ecotag -d $base_dir/"${base_pref}" -R $refdb_dir $all_sample_sequences_uniq > $all_sample_sequences_tag
-# Supression des attributs inutiles dans l'entête des séquences
+# Removal of unnecessary attributes in sequence headers
 all_sample_sequences_ann="${all_sample_sequences_tag/.fasta/.ann.fasta}"
 $obiannotate  --delete-tag=scientific_name_by_db --delete-tag=obiclean_samplecount \
  --delete-tag=obiclean_count --delete-tag=obiclean_singletoncount \
@@ -83,10 +108,10 @@ $obiannotate  --delete-tag=scientific_name_by_db --delete-tag=obiclean_samplecou
  --delete-tag=reverse_score --delete-tag=reverse_primer --delete-tag=reverse_match --delete-tag=reverse_tag \
  --delete-tag=forward_tag --delete-tag=forward_score --delete-tag=forward_primer --delete-tag=forward_match \
  --delete-tag=tail_quality --delete-tag=mode --delete-tag=seq_a_single $all_sample_sequences_tag > $all_sample_sequences_ann
-# Tri des séquences par 'count'
+# Sort sequences by 'count'
 all_sample_sequences_sort="${all_sample_sequences_ann/.fasta/.sort.fasta}"
 $obisort -k count -r $all_sample_sequences_ann > $all_sample_sequences_sort
-# Création d'un tableau final
+# Create the final table
 $obitab -o $all_sample_sequences_sort > $fin_dir/"$step".csv
 
 gzip $main_dir/*
