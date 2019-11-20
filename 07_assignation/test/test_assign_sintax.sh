fd5ba22a6fd250b5b3582dc56b972567a08f92bc
peguerin <pierre-edouard.guerin@cefe.cnrs.fr>
2019-11-20 15:25:03 +0100

    correct test path

--- 07_assignation/test/test_assign_sintax.sh
+++ 07_assignation/test/test_assign_sintax.sh
@@ -1,5 +1,4 @@
 source 98_infos/config.sh
-vsearch=${SINGULARITY_EXEC_CMD}" "${EDNATOOLS_SIMG}" vsearch"
 container_python2=${SINGULARITY_EXEC_CMD}" "${EDNATOOLS_SIMG}" python2"
 obitab=${SINGULARITY_EXEC_CMD}" "${OBITOOLS_SIMG}" obitab"
 usearch=${SINGULARITY_EXEC_CMD}" "${EDNATOOLS_SIMG}" usearch"
@@ -25,6 +24,6 @@ PREFINI=`pwd`"/07_assignation/test/02_sintax/PREFINI_sintax"
 
 $usearch -sintax $all_sample_sequences_sort_upper -db $refdb_dir -sintax_cutoff 0.8 -strand plus -tabbedout $ASSIGNED
 
-$container_python2 07_assignation/convert_assign_sintax_2_obifasta.py -f $all_sample_sequences_sort -a $ASSIGNED -o $PREFINI
+$container_python2 07_assignation/convert_assign_sintax_2_obifasta.py -f $all_sample_sequences_sort -s $ASSIGNED -o $PREFINI
 
 $obitab -o $PREFINI > sintax_test.csv
