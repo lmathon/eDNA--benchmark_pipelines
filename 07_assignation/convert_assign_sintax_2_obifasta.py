082a59eed18b4529887db75f5096fe1cc9a5aae2
peguerin <pierre-edouard.guerin@cefe.cnrs.fr>
2019-11-20 15:16:00 +0100

    convert sintax2fasta

--- 07_assignation/convert_assign_sintax_2_obifasta.py
+++ 07_assignation/convert_assign_sintax_2_obifasta.py
@@ -53,15 +53,22 @@ parser.add_argument("-f","--obifasta", type=str)
 #===============================================================================
 
 args = parser.parse_args()
-sintaxFile = args.vsearch_assignation
+sintaxFile = args.sintax_assignation
 outputFile = args.output
 obitFastaFile = args.obifasta
 
+
+#sintaxFile="07_assignation/test/02_sintax/assigned_sintax.csv"
+#obiFastaFile="07_assignation/test/02_sintax/grinder_teleo1_all_sample_clean.uniq.ann.sort.fasta"
+#outputFile="sintax_test.fasta"
+
 dicOfseq={}
+
+sintax_regex = re.compile('f:.*,g:.*,s:.*')
 
 
 ##  read vsearch assignement --blast6out
-with open(vsearchFile,'r') as readFile:
+with open(sintaxFile,'r') as readFile:
     for ligne in readFile.readlines():     
         ligneSplit=ligne.split(";")
         #print ligneSplit
@@ -73,16 +80,41 @@ with open(vsearchFile,'r') as readFile:
                 infoVal=elemSplit[1]
                 if infoTag == "merged_sample":
                     infoline+=infoTag+"="+infoVal+"; "
-                elif infoTag == "family_name":
-                    infoline+=infoTag+"="+infoVal+"; "
-                elif infoTag == "genus_name":
-                    infoline+=infoTag+"="+infoVal+"; "
-                elif infoTag == "species_name":
-                    infoline+=infoTag+"="+infoVal+"; "
-                elif infoTag == "rank":
-                    infoline+=infoTag+"="+infoVal+"; "
                 else:
                     infoline=infoline
+            else:
+            	if re.search(sintax_regex, elem) is not None:
+            		raw_assign= elem.replace("\t","").replace("+","")
+            		for lv in raw_assign.split(","):
+            			lvSplit=lv.split("(")[0].split(":")
+            			if len(lvSplit) == 2:
+            				assignTag=lvSplit[0]
+            				assignVal=lvSplit[1]
+            				if assignTag == "f" and "family_name" not in infoline:
+            					infoline+="family_name="+assignVal+"; "
+            				elif assignTag == "g" and "species_name" not in infoline:
+            					infoline+="genus_name="+assignVal+"; "
+            				elif assignTag == "s" and "species_name" not in infoline:
+            					infoline+="species_name="+assignVal+"; "
+            				else:
+            					infoline=infoline
         infoline=ligneSplit[0]+"; "+infoline
         #print infoline
         dicOfseq[ligneSplit[0].split(" ")[0]]=infoline
+
+
+
+mes_records=[]
+for seq_record in SeqIO.parse(obiFastaFile, "fasta", alphabet=IUPAC.unambiguous_dna):
+    seq_record_DescriptionSplit=seq_record.description.split(";")
+    vSeqId=str(seq_record_DescriptionSplit[0].split(" ")[0])    
+    if vSeqId in dicOfseq:
+        print vSeqId
+        local_id=dicOfseq[vSeqId]
+        local_seq=str(repr(str(seq_record.seq.lower()))).replace("'","")
+        local_record=SeqRecord(Seq(local_seq,IUPAC.unambiguous_dna), id=local_id,description="")
+        mes_records.append(local_record)
+        ## remove the key (we need only one sequence by key: the "seed" sequence)
+        del dicOfseq[vSeqId]
+
+SeqIO.write(mes_records, outputFile, "fasta")
