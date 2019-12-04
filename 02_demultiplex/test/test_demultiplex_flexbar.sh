6fe6ae3bf62c7306a1db7b1a235e02446dfefa33
peguerin <pierre-edouard.guerin@cefe.cnrs.fr>
2019-12-04 16:56:07 +0100

    reformat primer fasta files

--- 02_demultiplex/test/test_demultiplex_flexbar.sh
+++ 02_demultiplex/test/test_demultiplex_flexbar.sh
@@ -18,12 +18,18 @@ Primer_R=`pwd`"/02_demultiplex/Primer_R.fasta"
 ## flexbar - flexible barcode and adapter removal
 
 
-########## cette commande marche...
+## demultiplexing
 $flexbar -r $R1_fastq -p $R2_fastq --barcodes $Tags -t 02_demultiplex/test/02_flexbar/"$pref" -n 4
 
-######## non
-$flexbar -r $R1_fastq -p $R2_fastq --barcodes $Tags_F --barcodes2 $Tags_R -bt 0 -t 02_demultiplex/test/02_flexbar/"$pref" -n 4
-$flexbar -r $R1_fastq -p $R2_fastq --barcodes2 $Tags -bt 0 -t 02_demultiplex/test/02_flexbar/"$pref" -n 4
+
+
+
+
+######## cimetiere des commandes non fonctionnelles 
+#$flexbar -r $R1_fastq -p $R2_fastq -b $Tags -b2 $Tags -a $Primer_F -a2 $Primer_R -at 0.1 -t 02_demultiplex/test/02_flexbar/"$pref" -n 4
+#$flexbar -r $R1_fastq -p $R2_fastq --barcodes $Tags_F --barcodes2 $Tags_R -bt 0 -t 02_demultiplex/test/02_flexbar/"$pref" -n 4
+#$flexbar -r $R1_fastq -p $R2_fastq --barcodes2 $Tags -bt 0 -t 02_demultiplex/test/02_flexbar/"$pref" -n 4
+#$flexbar -p $R2_fastq --barcodes2 $Tags -t 02_demultiplex/test2/02_flexbar/"$pref" -n 4
 
 ## remove adapters
 $flexbar -r $R1_fastq -p $R2_fastq --adapters $Primer_F --adapters2 $Primer_R -bt 0 -t 02_demultiplex/test/02_flexbar/"$pref" -n 4
