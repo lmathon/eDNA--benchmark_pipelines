1c5bb93042a188d645101b25aea5bd6973a89995
peguerin <pierre-edouard.guerin@cefe.cnrs.fr>
2020-02-13 14:28:22 +0100

    merge sequence with same ids

--- 07_assignation/sintax2obitab.py
+++ 07_assignation/sintax2obitab.py
@@ -81,38 +81,55 @@ outputFile = args.output
 #sintaxFile="07_assignation/test/grinder_teleo1_all_sample_clean.uniq.ann.sort.uppercase.tag.fasta"
 #outputFile="07_assignation/test/tabfin.tsv"
 
+sintaxFile="/share/reservebenefit/working/pierre/eDNA--benchmark_pipelines/07_assignation/Outputs/02_sintax/main/grinder_teleo1_all_sample_clean.uniq.ann.sort.sintax_ann.csv"
 
 listOfLignes = []
+listOfIds = []
 
 with open(sintaxFile,'r') as readFile:
     for ligne in readFile.readlines():     
         ligneSplit=ligne.split(";")
         thisLigne= Ligne("NA","NA","NA","NA","NA","NA","NA")
         thisLigne.id_ligne=ligneSplit[0].split(" ")[0]
-        thisLigne.count=ligneSplit[0].split(" ")[1].split("=")[1]
-        #print(ligneSplit)
-        for elem in ligneSplit[1:]:
-            thisLigne.id_ligne=str(ligneSplit[0])
-            if "=" in elem:
-                elemSplit=elem.split("=")
-                if len(elemSplit) > 1:
-                    infoTag=elemSplit[0].replace(" ","")
-                    infoVal=elemSplit[1]
-                    if infoTag == "merged_sample":
-                        exec(elem.replace("\t"," ").replace(" ",""))
-                        thisLigne.merged_sample=merged_sample
-                    else:
-                        thisLigne.definition="."
-            else:
-                elemFormat=elem.replace("\t","").replace("\n","").lstrip().split(",")
-                for elemf in elemFormat:
-                    if elemf[0] =='f':
-                        thisLigne.family_name=elemf.split(':')[1].split('(')[0] #family
-                    elif elemf[0] == 'g':
-                        thisLigne.genus_name=elemf.split(':')[1].split('(')[0] #genus
-                    else:
-                        thisLigne.species_name=elemf.split(':')[1].split('(')[0] #species
-        listOfLignes.append(thisLigne)
+        if thisLigne.id_ligne not in listOfIds:
+            listOfIds.append(thisLigne.id_ligne)
+            thisLigne.count=ligneSplit[0].split(" ")[1].split("=")[1]        
+            for elem in ligneSplit[1:]:
+                thisLigne.id_ligne=str(ligneSplit[0])
+                if "=" in elem:
+                    elemSplit=elem.split("=")
+                    if len(elemSplit) > 1:
+                        infoTag=elemSplit[0].replace(" ","")
+                        infoVal=elemSplit[1]
+                        if infoTag == "merged_sample":
+                            exec(elem.replace("\t"," ").replace(" ",""))
+                            thisLigne.merged_sample=merged_sample
+                        else:
+                            thisLigne.definition="."
+                else:
+                    elemFormat=elem.replace("\t","").replace("\n","").lstrip().split(",")
+                    for elemf in elemFormat:
+                        if elemf[0] =='f':
+                            thisLigne.family_name=elemf.split(':')[1].split('(')[0] #family
+                        elif elemf[0] == 'g':
+                            thisLigne.genus_name=elemf.split(':')[1].split('(')[0] #genus
+                        else:
+                            thisLigne.species_name=elemf.split(':')[1].split('(')[0] #species
+                listOfLignes.append(thisLigne)
+        else:
+            for i in range(len(listOfLignes)):
+                if listOfLignes[i].id_ligne == thisLigne.id_ligne:
+                    for elem in ligneSplit[1:]:
+                        if "=" not in elem:
+                            elemFormat=elem.replace("\t","").replace("\n","").lstrip().split(",")
+                            for elemf in elemFormat:
+                                if elemf[0] =='f':
+                                    listOfLignes[i]=thisLigne.family_name+","+elemf.split(':')[1].split('(')[0] #family
+                                elif elemf[0] == 'g':
+                                    listOfLignes[i]=thisLigne.genus_name+","+elemf.split(':')[1].split('(')[0] #genus
+                                else:
+                                    listOfLignes[i]=thisLigne.species_name+","+elemf.split(':')[1].split('(')[0] #species
+
 
 ## get all keys of sample from merged_sample dic
 all_keys = list(set().union(*(d.merged_sample.keys() for d in listOfLignes)))
