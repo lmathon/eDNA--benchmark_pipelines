f64fb33b19a909c355514e1d6813ee7e1762e207
root <root@EB10PA4.newcefe.newage.fr>
2020-02-13 17:22:31 +0100

    update python scripts

--- optimized_pipeline/vsearch2obitab.py
+++ optimized_pipeline/vsearch2obitab.py
@@ -35,7 +35,6 @@ import argparse
 import os
 
 
-
 #===============================================================================
 #CLASS
 #===============================================================================
@@ -80,35 +79,66 @@ outputFile = args.output
 #vsearchFile="07_assignation/test/assign_vsearch.fasta"
 #outputFile="07_assignation/test/tabfin.tsv"
 
+#vsearchFile= "07_assignation/Outputs/01_vsearch/main/grinder_teleo1_all_sample_clean.uniq.ann.sort.uniqid.tag.fasta"
+
 
 listOfLignes = []
+listOfIds = []
+newId=1 # 1 -> this Id is new 0 -> this Id is already known
+
 ##  read vsearch assignement --blast6out
 with open(vsearchFile,'r') as readFile:
     for ligne in readFile.readlines():     
         ligneSplit=ligne.split(";")
         thisLigne= Ligne("NA","NA","NA","NA","NA","NA","NA")
-        for elem in ligneSplit[1:]:
-            thisLigne.id_ligne=str(ligneSplit[0])
-            elemSplit=elem.split("=")
-            if len(elemSplit) > 1:
-                infoTag=elemSplit[0].replace(" ","")
-                infoVal=elemSplit[1]
-                if infoTag == "merged_sample":
-                    exec(elem.replace("\t"," ").replace(" ",""))
-                    thisLigne.merged_sample=merged_sample
-                elif infoTag == "family_name":
-                    thisLigne.family_name=infoVal
-                elif infoTag == "genus_name":
-                    thisLigne.genus_name=infoVal
-                elif infoTag == "species_name":
-                    thisLigne.species_name=infoVal
-                elif infoTag == "count":
-                    thisLigne.count=infoVal
-                else:
+        thisLigne.id_ligne=str(ligneSplit[0].split(" ")[0].replace(">",""))
+        if thisLigne.id_ligne not in listOfIds:
+            listOfIds.append(thisLigne.id_ligne)
+            newId=1
+            thisCount=ligneSplit[0].split("=")[1]
+            thisLigne.count=thisCount
+            for elem in ligneSplit[1:]:            
+                elemSplit=elem.split("=")
+                if len(elemSplit) > 1:
+                    infoTag=elemSplit[0].replace(" ","")
+                    infoVal=elemSplit[1]
+                    if infoTag == "merged_sample":
+                        exec(elem.replace("\t"," ").replace(" ",""))
+                        thisLigne.merged_sample=merged_sample
+                    elif infoTag == "family_name":
+                        thisLigne.family_name=infoVal
+                    elif infoTag == "genus_name":
+                        thisLigne.genus_name=infoVal
+                    elif infoTag == "species_name":
+                        thisLigne.species_name=infoVal
+                    else:
+                        thisLigne.definition="."
                     thisLigne.definition="."
-                thisLigne.definition="."
-        listOfLignes.append(thisLigne)
-        
+        else:
+            newId=0
+            for i in range(len(listOfLignes)):
+                if listOfLignes[i].id_ligne == thisLigne.id_ligne:
+                    for elem in ligneSplit[1:]:            
+                        elemSplit=elem.split("=")
+                        if len(elemSplit) > 1:
+                            infoTag=elemSplit[0].replace(" ","")
+                            infoVal=elemSplit[1]                    
+                            if infoTag == "family_name":
+                                if listOfLignes[i].family_name != infoVal:                            
+                                    listOfLignes[i].family_name = listOfLignes[i].family_name + ',' +infoVal
+                            elif infoTag == "genus_name":
+                                if listOfLignes[i].genus_name != infoVal:                   
+                                    listOfLignes[i].genus_name = listOfLignes[i].genus_name + ',' +infoVal
+                            elif infoTag == "species_name":
+                                if listOfLignes[i].species_name != infoVal:                   
+                                    listOfLignes[i].species_name = listOfLignes[i].species_name + ',' +infoVal
+                            else:
+                                listOfLignes[i].definition="r"
+        if newId:
+            listOfLignes.append(thisLigne)
+
+
+
 ## get all keys of sample from merged_sample dic
 all_keys = list(set().union(*(d.merged_sample.keys() for d in listOfLignes)))
 
